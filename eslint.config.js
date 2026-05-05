0a38fbd711b0367162833a53fed5a9ad0d0db624
Melnikov Bogdan <72517532+MelnikovBogdan@users.noreply.github.com>
2026-05-05 11:26:10 +0300

    feat: add eslint prettier configs (#1)
    * feat: add eslint and prettier configs
    
    * fix: lint
    
    * fix: lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,40 @@
-import antfu from '@antfu/eslint-config'
+import globals from 'globals'
+import js from '@eslint/js'
+import pluginReact from 'eslint-plugin-react'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
+import tseslint from 'typescript-eslint'
+import prettierConfig from 'eslint-config-prettier'
+import pluginPrettier from 'eslint-plugin-prettier/recommended'
 
-export default antfu({
-    react: true,
-})
-
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.app.json', './tsconfig.node.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    ignores: ['node_modules/**', 'dist/**'],
+  },
+  prettierConfig,
+  pluginPrettier,
+)

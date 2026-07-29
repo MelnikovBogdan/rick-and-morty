a8ea16d3b630ca2f4b782087032ce8f26d0c2bde
Melnikov Bogdan <72517532+MelnikovBogdan@users.noreply.github.com>
2026-07-29 17:22:59 +0300

    cgore: configure routes

--- src/app/providers/router/router.ts
+++ src/app/providers/router/router.ts
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { Layout } from '@widgets/layout'
+import ErrorPage from '@pages/error'
 import NotFoundPage from '@pages/not-found'
 
 export const router = createBrowserRouter(
@@ -7,6 +8,7 @@ export const router = createBrowserRouter(
     {
       path: '/',
       Component: Layout,
+      ErrorBoundary: ErrorPage,
       children: [
         {
           index: true,
@@ -16,11 +18,11 @@ export const router = createBrowserRouter(
           path: 'characters/:characterId',
           lazy: () => import('@pages/character').then((m) => ({ Component: m.default })),
         },
+        {
+          path: '*',
+          Component: NotFoundPage,
+        },
       ],
-    },
-    {
-      path: '*',
-      element: <NotFoundPage />,
     },
   ],
   {

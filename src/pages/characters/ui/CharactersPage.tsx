8a1b9f1f5935bdaf27b1b8a65fd0f6b10464d614
Melnikov Bogdan <72517532+MelnikovBogdan@users.noreply.github.com>
2026-08-06 18:41:52 +0300

    fix: select popup styles

--- src/pages/characters/ui/CharactersPage.tsx
+++ src/pages/characters/ui/CharactersPage.tsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
+
 import { CharacterListCard, mockCharacters } from '@features/character'
 import RickAndMortyWelcome from '@shared/assets/images/RickAndMortyWelcome.png'
+import { Select } from '@shared/ui/select'
 
 import './CharactersPage.css'
 
 export function CharactersPage() {
+  const [value, setValue] = useState<string>()
+  const options = ['Option 1', 'Option 2', 'Option 3']
+
   return (
     <div className="characters-page">
+      <Select value={value} onValueChange={(v) => setValue(v)} options={options} />
+
+      <Select value={value} onValueChange={(v) => setValue(v)} options={options} size="sm" />
+
       <img
         src={RickAndMortyWelcome}
         alt="Rick and Morty welcome image"
